e6d572d7b59dc3bebd6884fe8f991c0890fde0d0
Toby Lovick <tcl44@cam.ac.uk>
2023-11-27 17:26:08 +0000

    Example Plotting

--- Example/Plotmaker.py
+++ Example/Plotmaker.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 from anesthetic import read_chains, make_2d_axes, make_1d_axes
 from anesthetic.read.getdist import read_paramnames
@@ -11,13 +10,13 @@ params,names = read_paramnames(rootfile)
 fig, axes = make_2d_axes(params, upper = False)
 
 
-samples.plot_2d(axes,c = '#CC3311',
+samples.plot_2d(axes,c = '#CC3311', #Plotting with recommended resolution settings
                       kind={'lower': 'kde_2d', 'diagonal': 'kde_1d'},
                       diagonal_kwargs={'nplot_1d': 1000},
                       lower_kwargs={'nplot_2d': 100**2},
                       ncompress='entropy')
                       
-for j in params:
+for j in params: #Adding Lines on the median of each 1d posterior
     median = samples[j].quantile(0.5)
     axes.axlines({j: median}, ls=':', c='black', lw = 0.7, lower=False)   
     
